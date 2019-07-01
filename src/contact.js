fb15e840e9abba1d61f7450a79f827473c4003bb
Yari PL <yaroslav.khudchenko@gmail.com>
2019-07-02 00:30:06 +0200

    clean

---
+++ src/contact.js
@@ -0,0 +1,72 @@
+import React from 'react';
+import './css/main.css';
+import axios from 'axios';
+
+
+class Contact extends React.Component {
+	state = {
+        name: '',
+        message: '',
+        email: '',
+        sent: false,
+        buttonText: 'Send Message'
+    }
+
+    formSubmit = (e) => {
+    	// stop page reloading
+	  e.preventDefault()
+
+	  this.setState({
+	      buttonText: 'Sending ...'
+	  })
+
+	  let data = {
+	      name: this.state.name,
+	      email: this.state.email,
+	      message: this.state.message
+	  }
+	  
+	  axios.post('API_URI', data)
+	  .then( res => {
+	      this.setState({ sent: true }, this.resetForm())
+	  })
+	  .catch( () => {
+	    console.log('Message not sent')
+	  })
+	}
+	// reset form after submiting and sending data
+	resetForm = () => {
+	    this.setState({
+	        name: '',
+	        message: '',
+	        email: '',
+	        buttonText: 'Message Sent'
+	    })
+	}
+
+	render() {
+		return(
+			<div id='contact'>
+				
+				<div className='formTitle'>
+					Contact
+				</div>
+				<form className="form" onSubmit={ (e) => this.formSubmit(e)}>
+				  <textarea onChange={e => this.setState({ message: e.target.value})} name="message" type="text" placeholder="message" value={this.state.message} required/>
+
+				  <input onChange={e => this.setState({ name: e.target.value})} name="name" type="text" placeholder="name" value={this.state.name}/>
+
+				  <input onChange={(e) => this.setState({ email: e.target.value})} name="email" type="email" placeholder="email" required value={this.state.email} />
+
+				  <div className="button">
+				      <button type="submit" className="button button-primary">{ this.state.buttonText }</button>
+				  </div>
+				</form>
+
+
+			</div>
+		)
+	}
+}
+
+export default Contact;
---
+++ src/portfolio.js
@@ -0,0 +1,116 @@
+import React from 'react';
+import './css/main.css';
+
+class Portfolio extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			//defaultSortValue: 'all',
+			currentSortData:[],
+			projectsData: [{
+				title:'project1',
+				category:['all','js','react'],
+				image:'./1.jpg'
+			},
+			{
+				title:'project2',
+				category:['all','js','node'],
+				image:'./2.jpg'
+			},
+			{
+				title:'project3',
+				category:['all','react','node'],
+				image:'./3.jpg'
+			}
+			,{
+				title:'project4',
+				category:['all','js'],
+				image:'./4.jpg'
+			}
+			,{
+				title:'project5',
+				category:['all','react'],
+				image:'./5.jpg'
+			}
+			,{
+				title:'project6',
+				category:['all','node'],
+				image:'./6.jpg'
+			}
+			,{
+				title:'project7',
+				category:['all','js'],
+				image:'./7.jpg'
+			}
+			,{
+				title:'project8',
+				category:['all','js','react'],
+				image:'./8.jpg'
+			}
+			,{
+				title:'project9',
+				category:['all','react'],
+				image:'./9.jpg'
+			}
+			,{
+				title:'project10',
+				category:['all','js','react'],
+				image:'./10.jpg'
+			}]
+		}
+		this.sortProjects = this.sortProjects.bind(this)
+	}
+
+	sortProjects(e) {
+		
+		let singleProject = null;
+		let singleProjectArray = [];
+		//console.log(this.state);
+		//console.log(`hello ${e}`);
+		for(let i=0;i<this.state.projectsData.length;i++){
+			for(let y=0;y<this.state.projectsData[i].category.length;y++){
+				//console.log(this.state.projectsData[i])
+				if(this.state.projectsData[i].category[y] === e) {
+
+					//console.log('yes');
+					singleProject = `<div class='singleProject'>
+					<div class='title'>${this.state.projectsData[i].title}</div>
+					<img src='${this.state.projectsData[i].image}'></div>`
+					// fullfill current data with good values
+					
+					singleProjectArray.push(singleProject);
+					
+				}
+			}
+		}
+		document.querySelector('.gallery').innerHTML = singleProjectArray.join('');
+	}
+	componentDidMount() {
+		// run initial gallery sort
+    	this.sortProjects('node');
+    }
+	render() {
+		return (
+			<div id='portfolio'>
+				<div className='selector'>
+					<div className='allP selectorButtons' onClick={() => this.sortProjects('all')}>
+					ALL
+					</div>
+					<div className='javascriptP selectorButtons' onClick={() => this.sortProjects('js')}>
+					Javascript
+					</div>
+					<div className='reactjsP selectorButtons' onClick={() => this.sortProjects('react')}>
+					React.js
+					</div>
+					<div className='nodejsP selectorButtons' onClick={() => this.sortProjects('node')}>
+					Node.js
+					</div>
+				</div>
+				<div className='gallery'>
+				</div>
+			</div>
+		)
+	}
+}
+
+export default Portfolio;
