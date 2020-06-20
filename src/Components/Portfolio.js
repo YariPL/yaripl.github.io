03a8c98e3747518dc9d0ec0a09fcaf1e4785a93b
Yari PL <yaroslav.khudchenko@gmail.com>
2020-06-20 19:55:10 +0200

    start working on the project descr

--- src/Components/Portfolio.js
+++ src/Components/Portfolio.js
@@ -3,34 +3,45 @@ import React, { useState } from 'react';
 import image1 from './../assets/project1.png';
 import image2 from "./../assets/project2.png";
 import image3 from "./../assets/project3.png";
-
+import ArrowForwardIosIcon from "@material-ui/icons/ArrowForwardIos";
 export const Portfolio = () => {
 	const [portfolioState, setPortfolioState] = useState({
-    projectsData: [
-      {
-        title: "project1",
-		color: "red",
-		image: image1
-      },
-      {
-        title: "project2",
-		color: "violet",
-		image: image2
-      },
-      {
-        title: "project3",
-		color: "green",
-		image: image3
-      },
-    ],
-  });
+		projectsData: [
+		{
+			title: "404 TREX 3d clone",
+			image: image1,
+		},
+		{
+			title: "Task manager",
+			image: image2,
+		},
+		{
+			title: "Pathfinding visualizer",
+			image: image3,
+		},
+		],
+	});
+	const [activeProject, setActiveProject] = useState([]);
+	const [showProjectDescr, setShowProjectDescr] = useState(false);
+	
 	return (
 		<div id='portfolio'>
 			<div className='gallery'>
 				{portfolioState.projectsData.map((singleProject, index) => 
 					<div key={index} className="singleProject">
-						<div className='bodyS' style={{background:singleProject.color}} alt="projectName">
-							<div className="title">{singleProject.title}</div>
+						<div className='bodyS' alt="projectName">
+							<div className="title">
+								<div className='titletitle'>
+									{singleProject.title}
+								</div>
+								<ArrowForwardIosIcon 
+									style={{transform:'rotate(90deg)'}}
+									onClick={()=>{
+										setActiveProject(singleProject)
+										setShowProjectDescr(true);
+									}}
+								/>
+							</div>
 							<div className='projectPreview'>
 								<img src={singleProject.image} alt='project preview' />
 							</div>
