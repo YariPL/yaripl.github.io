45d0e8a101af67a5a921577e34644d5c41d9ded6
Yari PL <yaroslav.khudchenko@gmail.com>
2020-06-21 10:23:31 +0200

    styling descr to single project

--- src/Components/Portfolio.js
+++ src/Components/Portfolio.js
@@ -3,6 +3,11 @@ import React, { useState } from 'react';
 import image1 from './../assets/project1.png';
 import image2 from "./../assets/project2.png";
 import image3 from "./../assets/project3.png";
+import react from './../assets/stack/react.png';
+import sass from './../assets/stack/sass.png';
+import mongodb from './../assets/stack/mongodb.png';
+import nodejs from './../assets/stack/nodejs.png';
+import project2Video from './../assets/videos/mov_bbb.mp4';
 import ArrowForwardIosIcon from "@material-ui/icons/ArrowForwardIos";
 export const Portfolio = () => {
 	const [portfolioState, setPortfolioState] = useState({
@@ -14,8 +19,8 @@ export const Portfolio = () => {
       {
         title: "Task manager",
         image: image2,
-        techStack: ["react", "scss", "mongodb", "nodejs", "express"],
-        videoUrl: "https://www.youtube.com/embed/noneMROp_E8"
+        techStack: [react, sass, mongodb, nodejs /* , "express" */],
+        videoUrl: project2Video,
       },
       {
         title: "Pathfinding visualizer",
@@ -27,38 +32,56 @@ export const Portfolio = () => {
 	const [showProjectDescr, setShowProjectDescr] = useState(false);
 	
 	return (
-		<div id='portfolio'>
-			<div className='gallery'>
-				{portfolioState.projectsData.map((singleProject, index) => 
-					<div key={index} className="singleProject">
-						<div className='bodyS' alt="projectName">
-							<div className="title">
-								<div className='titletitle'>
-									{singleProject.title}
-								</div>
-								<ArrowForwardIosIcon 
-									style={{transform:'rotate(90deg)'}}
-									onClick={()=>{
-										setActiveProject(singleProject)
-										setShowProjectDescr(true);
-									}}
-								/>
-							</div>
-							<div className='projectPreview'>
-								<img src={singleProject.image} alt='project preview' />
-							</div>
-						</div>
-						{showProjectDescr &&
-							<div className='bodyD'>
-
-										
-
-							</div>
-						}
-					</div>
-				)}
-			</div>
-		</div>
-	)
+    <div id="portfolio">
+      <div className="gallery">
+        <div className="projects">
+          {portfolioState.projectsData.map((singleProject, index) => (
+            <div key={index} className="singleProject">
+              <div className="bodyS" alt="projectName">
+                <div className="title">
+                  <div className="titletitle">{singleProject.title}</div>
+                  <ArrowForwardIosIcon
+                    style={{ transform: "rotate(90deg)" }}
+                    onClick={() => {
+                      setActiveProject(singleProject);
+                      setShowProjectDescr(true);
+                    }}
+                  />
+                </div>
+                <div className="projectPreview">
+                  <img src={singleProject.image} alt="project preview" />
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
+        {showProjectDescr && (
+          <div className="bodyD">
+            <div className="bodyDVideo">
+              <video controls>
+                <source src={activeProject.videoUrl} type="video/mp4" />
+              </video>
+            </div>
+            <div className="bodyDDescr">
+              <div className="bodyDDescrDescr">
+                Task manager project build to control multiple projects with
+                tasks and subtasks. All changes done by user will be reflected
+                immediately.
+              </div>
+              <div className="bodyDDescrStack">
+                {activeProject.techStack.map((e, index) => (
+                  <img src={e} alt="stack" key={index} />
+                ))}
+              </div>
+              <div className="bodyDDescrCheck">
+                <div className="checkCode">Code</div>
+                <div className="checkLive">Live</div>
+              </div>
+            </div>
+          </div>
+        )}
+      </div>
+    </div>
+  );
 }
 
