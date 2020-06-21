76016a4d5b408e5acdb932bf948a6ac032753c08
Yari PL <yaroslav.khudchenko@gmail.com>
2020-06-21 17:14:02 +0200

    again back to burger

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -12,26 +12,34 @@ export const Header = () => {
                 menu.classList.replace('hideMenu', 'showMenu')
 
         }
-    useEffect(()=>{
+    /* useEffect(()=>{
         setTimeout(() => {
             document.querySelector('.menu').style.opacity = 1;
         }, 1500);
-    }, [])
+    }, []) */
     
     return (
-        <div id="header">
-            <div className="burger" onClick={toggleBurgerMenu}>
-                <div className="bar1"></div>
-                <div className="bar2"></div>
-                <div className="bar3"></div>
-            </div>
-
-            <div className='menu'>
-                <div className='menuItem'><a href='#homePage'>Home</a></div>
-                <div className='menuItem'><a href='#about'>About</a></div>
-                <div className='menuItem'><a href='#portfolio'>Portfolio</a></div>
-                <div className='menuItem'><a href='#contact'>Contact</a></div>
-            </div>
+      <div id="header">
+        <div className="burger" onClick={toggleBurgerMenu}>
+          <div className="bar1"></div>
+          <div className="bar2"></div>
+          <div className="bar3"></div>
         </div>
+
+        <div className="menu hideMenu">
+          <div className="menuItem">
+            <a href="#homePage">Home</a>
+          </div>
+          <div className="menuItem">
+            <a href="#about">About</a>
+          </div>
+          <div className="menuItem">
+            <a href="#portfolio">Portfolio</a>
+          </div>
+          <div className="menuItem">
+            <a href="#contact">Contact</a>
+          </div>
+        </div>
+      </div>
     );
 }
--- src/threejslogic/background.js
+++ src/threejslogic/background.js
@@ -1,36 +1,36 @@
 import React from 'react';
 import * as THREE from "./three.module.js";
 const Background = () => {
-	let camera, scene, renderer;
-	camera = new THREE.PerspectiveCamera(
-    70,
-    window.innerWidth / window.innerHeight,
-    0.01,
-    10
-  );
-  camera.position.z = 1;
+	let camera = new THREE.PerspectiveCamera(
+    	70,
+    	window.innerWidth / window.innerHeight,
+    	0.01,
+    	10
+  	);
+  	camera.position.z = 1;
 
 	// create scene
-	scene = new THREE.Scene();
+	let scene = new THREE.Scene();
 
 	let geometry = new THREE.BoxGeometry(0.2, 0.2, 0.2);
-  let material = new THREE.MeshNormalMaterial();
+	let material = new THREE.MeshNormalMaterial();
 
-  let mesh = new THREE.Mesh(geometry, material);
-  scene.add(mesh);
+	let mesh = new THREE.Mesh(geometry, material);
+	scene.add(mesh);
 
 
-	renderer = new THREE.WebGLRenderer({
+	let renderer = new THREE.WebGLRenderer({
 		antialias: true
 	});
 
 	renderer.setClearColor(0x273043); // to have light background color
-renderer.setSize( window.innerWidth, window.innerHeight );
-	document.body.appendChild( renderer.domElement );
+	renderer.setSize( window.innerWidth, window.innerHeight );
+	document.body.appendChild( renderer.domElement ); // append canvas to the body
 
+	window.addEventListener("resize", onWindowResize, false); // resizer for 3d scene
 
-	window.addEventListener("resize", onWindowResize, false);
-	animate();
+	animate(); // loop for 3d scene
+	
 	function animate() {
 		requestAnimationFrame(animate);
 
