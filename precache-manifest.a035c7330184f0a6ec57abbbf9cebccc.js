6cd2f0999caa0e81a7d131ddeb6fb31247c98811
Yari PL <yaroslav.khudchenko@gmail.com>
2020-06-22 17:41:45 +0200

    Updates

---
+++ precache-manifest.a035c7330184f0a6ec57abbbf9cebccc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1fd3a613317049beb0075948d4e97955",
+    "url": "/index.html"
+  },
+  {
+    "revision": "09c9571ae1e979d4bb28",
+    "url": "/static/css/main.2373061c.chunk.css"
+  },
+  {
+    "revision": "de3f2a04a5396f2c23ef",
+    "url": "/static/js/2.14c0ae2f.chunk.js"
+  },
+  {
+    "revision": "7e402396ca428a5bd514c48df615b24e",
+    "url": "/static/js/2.14c0ae2f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "09c9571ae1e979d4bb28",
+    "url": "/static/js/main.df539680.chunk.js"
+  },
+  {
+    "revision": "b4ba650b238755144e2a",
+    "url": "/static/js/runtime-main.e5fc83a6.js"
+  },
+  {
+    "revision": "198918f40ecc7cab0fc4231adaf67c96",
+    "url": "/static/media/mov_bbb.198918f4.mp4"
+  }
+]);
