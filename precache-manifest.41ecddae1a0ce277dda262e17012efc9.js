515b04ec85867ef5a7aad105cdbe7050d6222dd5
Yari PL <yaroslav.khudchenko@gmail.com>
2020-06-22 17:47:13 +0200

    Updates

---
+++ precache-manifest.41ecddae1a0ce277dda262e17012efc9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9a6f3d19dda6a61073d3bbe14af393ab",
+    "url": "/portfolio-page/index.html"
+  },
+  {
+    "revision": "c665ca85ea4d2a66801d",
+    "url": "/portfolio-page/static/css/main.2da5d344.chunk.css"
+  },
+  {
+    "revision": "de3f2a04a5396f2c23ef",
+    "url": "/portfolio-page/static/js/2.14c0ae2f.chunk.js"
+  },
+  {
+    "revision": "7e402396ca428a5bd514c48df615b24e",
+    "url": "/portfolio-page/static/js/2.14c0ae2f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c665ca85ea4d2a66801d",
+    "url": "/portfolio-page/static/js/main.6769bb9d.chunk.js"
+  },
+  {
+    "revision": "c502e07e22aae8a84536",
+    "url": "/portfolio-page/static/js/runtime-main.131e381a.js"
+  },
+  {
+    "revision": "198918f40ecc7cab0fc4231adaf67c96",
+    "url": "/portfolio-page/static/media/mov_bbb.198918f4.mp4"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.ea93b226cb3d17e53cc00bf71c71d6f2.js"
+  "/portfolio-page/precache-manifest.41ecddae1a0ce277dda262e17012efc9.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/portfolio-page/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
